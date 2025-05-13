f5edeb79f8fba88cee3f41a45e579e9eae1865e0
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-05-13 16:15:08 +0900

    Update app.js

--- advanced-integration/public/app.js
+++ advanced-integration/public/app.js
@@ -23,7 +23,7 @@ async function setupApplepay() {
 
     const paymentRequest = {
       countryCode,
-      currencyCode: 'USD',
+      currencyCode: 'UGX',
       merchantCapabilities,
       supportedNetworks,
       requiredBillingContactFields: [
