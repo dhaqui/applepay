1d272b7e889c2137b569a15255a833f72fa80d8a
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-05-13 18:21:36 +0900

    Update app.js

--- advanced-integration/public/app.js
+++ advanced-integration/public/app.js
@@ -23,7 +23,7 @@ async function setupApplepay() {
 
     const paymentRequest = {
       countryCode,
-      currencyCode: 'ZAR',
+      currencyCode: 'XOF',
       merchantCapabilities,
       supportedNetworks,
       requiredBillingContactFields: [
